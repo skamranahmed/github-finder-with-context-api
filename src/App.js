6ab69ed89e8861e4385dbae0e36b7c086ebb363f
skahmed <skahmed@myamu.ac.in>
2021-05-08 19:56:22 +0530

    User Item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
+import UserItem from "./components/users/UserItem";
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <Navbar />
+        <UserItem />
       </div>
     );
   }
