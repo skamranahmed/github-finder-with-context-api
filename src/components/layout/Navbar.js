5ef747acc63c3ec9e7f1f12f2e8888542a0bf4aa
skahmed <skahmed@myamu.ac.in>
2021-05-08 22:44:26 +0530

    Changed class based components to functional componenets

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,20 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class Navbar extends Component {
-  static defaultProps = {
-    title: "Github Finder",
-    icon: "fab fa-github",
-  };
+const Navbar = (props) => {
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={props.icon} /> {props.title}
+      </h1>
+    </nav>
+  );
+};
 
-  render() {
-    return (
-      <nav className="navbar bg-primary">
-        <h1>
-          <i className={this.props.icon} /> {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+Navbar.defaultProps = {
+  title: "Github Finder",
+  icon: "fab fa-github",
+};
 
 export default Navbar;
