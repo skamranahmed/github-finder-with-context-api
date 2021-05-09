a206e14bb01e63709a1f641b514d059ef36857b9
skahmed <skahmed@myamu.ac.in>
2021-05-10 05:13:14 +0530

    Added the About component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
 
 class App extends Component {
   state = {
@@ -67,6 +68,7 @@ class App extends Component {
                   </Fragment>
                 )}
               />
+              <Route exact path="/about" component={About} />
             </Switch>
           </div>
         </div>
